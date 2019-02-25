02ee2673c55cdbc06014be95085dea57d197e493
Michał Bukowski <michal.bukowski@kontomatik.com>
2019-02-25 12:09:29 +0100

    Solved 1 with constant time solution

--- lib/1/m35.ex
+++ lib/1/m35.ex
@@ -1,10 +1,12 @@
 defmodule M35 do
-	defp mod(n, d), do: rem(n, d)
-	defp mod3(n), do: mod(n, 3)
-	defp mod5(n), do: mod(n, 5)
+	defp sum_up_to(n), do: div(((n+1) * n), 2)
+	defp partial_result(n, d), do: n |> div(d) |> sum_up_to |> Kernel.*(d)
 	def of(n) do
-		1..n-1 
-		|> Enum.filter(fn x -> mod5(x) == 0 || mod3(x) == 0 end)
-		|> Enum.sum
+		with first = partial_result(n-1, 3),
+			 second = partial_result(n-1, 5),
+			 third = partial_result(n-1, 15)
+		do
+			first + second - third
+		end
 	end
 end
--- test/m35_test.exs
+++ test/m35_test.exs
@@ -2,7 +2,21 @@ defmodule M35Test do
   use ExUnit.Case
   doctest M35
 
+  defp mod(n, d), do: rem(n, d)
+	defp mod3(n), do: mod(n, 3)
+	defp mod5(n), do: mod(n, 5)
+	def naive_of(n) do
+		1..n-1 
+		|> Stream.filter(&mod5(&1) == 0 || mod3(&1) == 0)
+		|> Enum.sum
+	end
+
   test "sanity check" do
     assert M35.of(10) == 23
+  end
+
+  test "complex test" do
+  	1..150
+  	|> Enum.each(fn x -> assert M35.of(x) == M35Test.naive_of(x) end)
   end
 end
